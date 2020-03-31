090f1d4e5af2d9043504fe9749b3f5415eca831e
Siddharth <mail@siddharthjha.com>
2020-03-31 15:21:44 +0530

    fixed mobile video overflow issue

--- app.js
+++ app.js
@@ -17,4 +17,25 @@ modeSwitch.addEventListener('click', () => {
     document.body.classList.toggle('light');
     document.body.classList.toggle('dark');
     modeSwitch.classList.toggle('on');
-})
+});
+
+
+// I had to add this part below because I couldn't get the video to scale correctly on mobile devices
+// My temporary solution to that is to simply change the source of the video on width < 800px
+
+
+function changeVideoSource() {
+    if(window.innerWidth <= 800){
+        document.getElementsByTagName('video')[0].src = "assets/featured-video-mobile.mp4" // portrait video
+    } else {
+        document.getElementsByTagName('video')[0].src = "assets/featured-video.mp4" // landscape video
+    }
+}
+
+
+window.addEventListener("resize", changeVideoSource);
+
+// the above line does work, but cause flickering as the browser is being resized.
+// If I was deploying to production, I think I would omit the line and maybe use a variable to detect the state change
+
+changeVideoSource();
